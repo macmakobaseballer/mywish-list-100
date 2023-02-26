3c8c2d6e4cb7da5f1416a1b0678c2a5e78b0813b
山口諒 <>
2023-02-26 18:36:47 +0900

    wishes_ model.py

--- backend/app/models/wishes.py
+++ backend/app/models/wishes.py
@@ -1,2 +1,21 @@
 from datetime import datetime
 
+from pynamodb.models import Model
+from pynamodb.attributes import (
+    UnicodeAttribute, NumberAttribute, UTCDateTimeAttribute
+)
+
+class Wishes(Model):
+    class Meta:
+        table_name = 'wishes'
+        region = 'ap-northeast-1'
+        host = 'http://localhost:8000'
+        write_capacity_units = 10
+        read_capacity_units = 10
+    account_id =  UnicodeAttribute(hash_key=True)
+    wish_id = NumberAttribute(range_key=True)
+    title = UnicodeAttribute(null=True)
+    detail = UnicodeAttribute(null=True)
+    status = UnicodeAttribute
+    created_at = UTCDateTimeAttribute
+    updated_at = UTCDateTimeAttribute
